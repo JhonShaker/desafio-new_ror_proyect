e37f3a71737e6afc7b90cf251572cc4d5e86bc3e
JhonShaker <juan.ferrada.1991@gmail.com>
2023-06-19 23:23:01 -0400

    Estructura de paginas, controladores, relacion entre paginas y desarrollo de vistas requeridas.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/projects'
+  get 'pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get "/home", to: 'pages#home'
+  get "/projects", to: 'pages#projects'
+  get "/contact", to: 'pages#contact'
+  root "pages#home"
 end
